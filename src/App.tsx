dcc8bcd9646157fb09598fa2c7b73684d44a6676
Srinivaskoruprolu007 <srinivaskoruprolu5@gmail.com>
2025-01-11 20:18:15 +0530

    initial commit and add needed components from shadcn

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <div>
+      <Button variant={"destructive"}>hello</Button>
+    </div>
+  );
+}
+
+export default App;
