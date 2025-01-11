5e2f16818edcaf5ccdbae83433335d1da92a9be5
Srinivaskoruprolu007 <srinivaskoruprolu5@gmail.com>
2025-01-11 22:55:46 +0530

    add theme toggler using shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
-import { BrowserRouter } from "react-router-dom";
-import { ThemeProvider } from "./components/theme-provider";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "./components/context/theme-provider";
 import Layout from "./components/Layout";
+import WeatherDashboard from "./pages/weather-dashboard";
+import CityPage from "./pages/city-page";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools"
 
+const queryClient = new QueryClient();
 function App() {
   return (
-    <BrowserRouter>
-      <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-        <Layout>
-          Hello
-        </Layout>
-      </ThemeProvider>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+          <Layout>
+            <Routes>
+              <Route path="/" element={<WeatherDashboard />} />
+              <Route path="/city/:cityName" element={<CityPage />} />
+            </Routes>
+          </Layout>
+        </ThemeProvider>
+      </BrowserRouter>
+      <ReactQueryDevtools initialIsOpen={false} / >
+    </QueryClientProvider>
   );
 }
 
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,11 +1,18 @@
 import { PropsWithChildren } from "react";
+import Header from "./Header";
 
 const Layout = ({ children }: PropsWithChildren) => {
   return (
-    <div>
-      <header></header>
-      {children}
-      <footer></footer>
+    <div className="dark:bg-gray-800">
+      <Header />
+      <main className="min-h-screen container mx-auto px-4 py-8">
+        {children}
+      </main>
+      <footer className="border-t  py-12 ">
+        <div className="container mx-auto text-center px-4 text-gray-400">
+          <p>Made with ❤️ by Srinivas</p>
+        </div>
+      </footer>
     </div>
   );
 };
