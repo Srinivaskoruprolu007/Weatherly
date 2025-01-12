c9f80184a58a51bd804ba6b2f500d3e2581e9e9f
Srinivaskoruprolu007 <srinivaskoruprolu5@gmail.com>
2025-01-12 17:29:49 +0530

    update app

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import Layout from "./components/Layout";
 import WeatherDashboard from "./pages/weather-dashboard";
 import CityPage from "./pages/city-page";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "sonner";
 
 const queryClient = new QueryClient({
