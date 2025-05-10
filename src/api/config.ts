5a6c0747309fa45badbe68afb8f67fa676e2f247
Srinivaskoruprolu007 <srinivaskoruprolu5@gmail.com>
2025-05-10 07:10:14 +0530

    fix(config): update GEO_CODING URL to use HTTPS
    The GEO_CODING URL was updated from HTTP to HTTPS to ensure secure communication with the API. This change enhances security by encrypting data in transit.

--- src/api/config.ts
+++ src/api/config.ts
@@ -1,6 +1,6 @@
 export const API_CONFIG = {
   BASE_URL: "https://api.openweathermap.org/data/2.5",
-  GEO_CODING: "http://api.openweathermap.org/geo/1.0",
+  GEO_CODING: "https://api.openweathermap.org/geo/1.0",
   API_KEY: import.meta.env.VITE_WEATHER_API,
   DEFAULT_PARAMS: {
     units: "metric",
